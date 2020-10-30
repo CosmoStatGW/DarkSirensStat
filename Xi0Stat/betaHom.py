Fixed catalog averaging. Performance improvements. Adjusted LoadCompleteness.

Xi0Stat/betaHom.py
@@ -1,42 +1,42 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Sep 23 10:36:12 2020
-
-@author: Michi
-"""
-
-####
-# This module contains a class to compute beta from the assumption of homogeneous galaxy distribution
-####
-
-
-from Xi0Stat.beta import Beta
-from Xi0Stat.globals import *
-from scipy.integrate import quad
-
-class BetaHom(Beta):
-    
-    def __init__(self, **kwargs):
-        Beta.__init__(self, **kwargs)
-        
-        
-    def get_beta(self, H0, Xi0, dMax, zR=zRglob, n=1.91, ):
-        '''
-        Homogeneous beta -  Eq. 2.81 
-        '''
-        #  dmax is the maximum value of the luminosity distance 
-        # (averaged over the solid angle and over the θ′ parameters, 
-        # so in particular over the source inclination) 
-        # to which the given monochromatic population of source could be detected,
-        # given the sensitivity of the detector network
-        # zMax is the corespondig
-        
-        zMax = z_from_dLGW(dMax, H0,  Xi0, n=n)
-        cosmo=FlatLambdaCDM(H0=H0, Om0=Om0GLOB)
-                
-        norm = quad(lambda x: cosmo.differential_comoving_volume(x).value, 0, zR )[0]
-        num = quad(lambda x:  cosmo.differential_comoving_volume(x).value, 0, zMax )[0]
-        return num/norm
-        
-        return beta
+##!/usr/bin/env python3
+## -*- coding: utf-8 -*-
+#"""
+#Created on Wed Sep 23 10:36:12 2020
+#
+#@author: Michi
+#"""
+#
+#####
+## This module contains a class to compute beta from the assumption of homogeneous galaxy distribution
+#####
+#
+#
+#from Xi0Stat.beta import Beta
+#from Xi0Stat.globals import *
+#from scipy.integrate import quad
+#
+#class BetaHom(Beta):
+#    
+#    def __init__(self, **kwargs):
+#        Beta.__init__(self, **kwargs)
+#        
+#        
+#    def get_beta(self, H0, Xi0, dMax, zR=zRglob, n=1.91, ):
+#        '''
+#        Homogeneous beta -  Eq. 2.81 
+#        '''
+#        #  dmax is the maximum value of the luminosity distance 
+#        # (averaged over the solid angle and over the θ′ parameters, 
+#        # so in particular over the source inclination) 
+#        # to which the given monochromatic population of source could be detected,
+#        # given the sensitivity of the detector network
+#        # zMax is the corespondig
+#        
+#        zMax = z_from_dLGW(dMax, H0,  Xi0, n=n)
+#        cosmo=FlatLambdaCDM(H0=H0, Om0=Om0GLOB)
+#                
+#        norm = quad(lambda x: cosmo.differential_comoving_volume(x).value, 0, zR )[0]
+#        num = quad(lambda x:  cosmo.differential_comoving_volume(x).value, 0, zMax )[0]
+#        return num/norm
+#        
+#        return beta

Xi0Stat/betaMC.py
@@ -9,7 +9,9 @@ from Xi0Stat.globals import *
 
 class BetaMC:#(Beta):
     
-    def __init__(self, priorlimits, nSamples=1000000, observingRun = 'O2', SNRthresh = 8, verbose=False, **kwargs):
+    def __init__(self, priorlimits, nSamples=1000000, observingRun = 'O2', SNRthresh = 8, properAnisotropy = True, selectionFunc = None, verbose=False, **kwargs):
+    
+        self._properAnisotropy = properAnisotropy
         self._observingRun = observingRun
         
         self.verbose = verbose
@@ -25,6 +27,7 @@ class BetaMC:#(Beta):
         import os
         if self._observingRun == 'O2':
             filename = '2017-08-06_DCH_C02_L1_O2_Sensitivity_strain_asd.txt'
+            #filename = '2016-12-13_C01_L1_O2_Sensitivity_strain_asd.txt'
         elif self._observingRun == 'O3':
             filename = '2018-10-20_DELTAL_FE_L1_O3_Sensitivity_strain_asd.txt'
             
@@ -144,7 +147,14 @@ class BetaMC:#(Beta):
         phisample = 2*np.pi*np.random.uniform(size=self.nSamples)
         #pdf is sin(theta), cdf is (1-cos(theta))/2, inverse cdf is arccos(1-2*x)
         # ----no, actually only need cos of these angles which are uniform as they should
+        #costhetasample = 0.1 -0.2*np.random.uniform(size=self.nSamples)
         costhetasample = 1-2*np.random.uniform(size=self.nSamples)
+        tsample = np.random.uniform(size=self.nSamples)
+        
+        costhetaL, phiL = self._equat2detector('livingston', costhetasample, phisample, tsample)
+        costhetaH, phiH = self._equat2detector('hanford', costhetasample, phisample, tsample)
+        
+        
         cosinclsample = 1-2*np.random.uniform(size=self.nSamples)
         
         cosiota = cosinclsample
@@ -153,12 +163,20 @@ class BetaMC:#(Beta):
         # polarization in plane perpendicular to propagagion
         # from matching detector polarization reference and source (aligned to major and minor axis)
         # drops out in sum of squares (7.271) (ok: is also mentioned in a side note)
-        Fp = 0.5*(1+costhetasample**2)*np.cos(2*phisample)
-        Qp = Fp*0.5*(1+cosinclsample**2)
-        Fc = costhetasample*np.sin(2*phisample)
-        Qc = Fc*cosinclsample
-        # (7.178f)
-        Qsq = Qp**2 + Qc**2
+        
+        def Qsq(costh, phi, cosincl):
+        
+            Fp = 0.5*(1+costh**2)*np.cos(2*phi)
+            Qp = Fp*0.5*(1+cosincl**2)
+            Fc = costh*np.sin(2*phi)
+            Qc = Fc*cosincl
+            # (7.178f)
+            return Qp**2 + Qc**2
+            
+        QsqL = Qsq(costhetaL, phiL, cosinclsample)
+        QsqH = Qsq(costhetaH, phiH, cosinclsample)
+        
+        Qsq = np.minimum(QsqL, QsqH)
         
         GMsun_over_c3 = 4.927e-6# seconds
         clightMpc = clight/3.086e+19 #km/s -> Mpc/s
@@ -168,10 +186,80 @@ class BetaMC:#(Beta):
         SNR = 1/np.pi**(2/3)*(GMsun_over_c3*Mc)**(2.5/3)*(clightMpc/dist_true)*np.sqrt(5/6*Qsq*np.interp(fGW, self.freq, self.integr))
         
         ## SELECTION ###############
-        mask = SNR > self.SNRthresh
+       
+        mask = (SNR > self.SNRthresh) #& (costhetasample < 0.1) & (costhetasample > -0.1)
+        mask = mask & (z_from_dLGW_fast(dist_true, H0=70, Xi0=1, n=nGlob) < 0.2) 
+        
         SNR = SNR[mask]
         if self.verbose:
             if np.sum(mask > 0):
                 print("Largest distance detected in sample is " + str(dist_true[mask].max()))
         return SNR.size/self.nSamples
 
+
+
+    def _equat2detector(self, detector, costheta, phi, t):
+        
+        if self._properAnisotropy == False:
+        
+            return costheta, phi
+        
+        else:
+        
+            trafos = np.zeros((t.size,3,3))
+            
+            if detector == 'livingston':
+            
+                c = np.cos(-0.0135118 + 2*np.pi*t)
+                s = -np.sin(-0.0135118 + 2*np.pi*t)
+                
+                trafos[:,0,0] = -0.15713*c +  0.951057*s
+                trafos[:,0,1] =  0.951057*c + 0.15713*s
+                trafos[:,0,2] = -0.266086
+                
+                trafos[:,1,0] = -0.483595*c - 0.309017*s
+                trafos[:,1,1] = -0.309017*c + 0.483595*s
+                trafos[:,1,2] = -0.818929
+                
+                trafos[:,2,0] = -0.861073*c
+                trafos[:,2,1] =  0.861073*s
+                trafos[:,2,2] =  0.508482
+                
+            elif detector == 'hanford':
+            
+                c = np.cos(0.513263 + 2*np.pi*t)
+                s = -np.sin(0.513263 + 2*np.pi*t)
+                
+                trafos[:,0,0] = 0.586405*c + 0.587785*s
+                trafos[:,0,1] = 0.587785*c - 0.586405*s
+                trafos[:,0,2] = 0.557348
+                
+                trafos[:,1,0] = -0.426048*c + 0.809017*s
+                trafos[:,1,1] = 0.809017*c + 0.426048*s
+                trafos[:,1,2] = -0.404937
+                
+                trafos[:,2,0] = -0.688921*c
+                trafos[:,2,1] =  0.688921*s
+                trafos[:,2,2] =  0.724837
+     
+            sintheta = np.sqrt(1-costheta**2)
+            x = np.cos(phi)*sintheta
+            y = np.sin(phi)*sintheta
+            z = costheta
+            
+            dir = (np.vstack([x,y,z]).T)[:,np.newaxis,:]
+            
+            res = np.sum(dir*trafos, axis=2)
+            
+            costhetanew = np.clip(res[:, 2], a_min=0, a_max=1)
+            sinthetanew = np.sqrt(1-costhetanew**2)
+            xnew = res[:, 0]
+            ynew = res[:, 1]
+        
+            cosphinew = np.clip(xnew/sinthetanew, a_min=0, a_max=1)
+            phinew = np.arccos(cosphinew)
+
+            return costhetanew, phinew
+            
+           
+          

Xi0Stat/completeness.py
@@ -555,39 +555,43 @@ class LoadCompleteness(SuperpixelCompleteness):
     def __init__(self, fname, comovingDensityGoal, interpolateOmega, **kwargs):
         import os
         filepath = os.path.join(dirName, 'data', 'GLADE', fname)
+        
+        self.filepath = filepath
 
-        self.fname = filepath
-        #f = open(self.fname)
         f = open(filepath)
         header = f.readline()
         f.close()
-        self.zMin = float(header.split(',')[1].split('=')[1])
-        self.zMax = float(header.split(',')[2].split('=')[1])
+        
         nside = int(header.split(',')[3].split('=')[1])
-        angularRes = int(np.log2(nside))
+        angularRes = int(np.log2(nside)+1e-5)
         zRes = int(header.split(',')[4].split('=')[1])
-
+        
+        print(nside, 2**angularRes, zRes)
+        
         SuperpixelCompleteness.__init__(self, comovingDensityGoal, angularRes, zRes, interpolateOmega, **kwargs)
-
+        
+        zcentermin = float(header.split(',')[1].split('=')[1])
+        zcentermax = float(header.split(',')[2].split('=')[1])
+        
+        self.zcenters = np.linspace(zcentermin, zcentermax, zRes)
+        deltaz = self.zcenters[1]-self.zcenters[0]
+        self.zMin = self.zcenters[0] - deltaz*0.5
+        self.zMax = self.zcenters[-1] + deltaz*0.5
+        
+        self.zedges = np.linspace(self.zMin, self.zMax, zRes + 1)
         
 
+    
+
     def compute_implementation(self, galdata, useDirac):
-
-        # Set _map, _zstar, zcenters, zedges
-
-
-        #zmax = 1.5*np.quantile(galdata.z.to_numpy(), 0.9)
-        self.zedges  = np.linspace(self.zMin, self.zMax, self._zRes+1)
 
         z1 = self.zedges[:-1]
         z2 = self.zedges[1:]
 
-        self.zcenters = 0.5*(z1 + z2)
-
         # LOAD MAP
         if self.verbose:
-            print('Loading copleteness from %s...' %self.fname)
-        self._map = np.loadtxt(self.fname).T
+            print('Loading copleteness from %s...' %self.filepath)
+        self._map = np.loadtxt(self.filepath).T
         #print(self._map[:2])
 
         if self.verbose:

Xi0Stat/galCat.py
@@ -238,6 +238,8 @@ class GalCompleted(object):
         
         # iterate through catalogs and add results to lists
         
+        catweightTotal = 0
+        
         for c, w in zip(self._galcats, self._catweights):
         
             # shorthand
@@ -253,6 +255,7 @@ class GalCompleted(object):
             allpixels.append(d[pixname].to_numpy())
             
             # keelin weights. N has to be tuned for speed vs quality
+            # for each gal, on zGrid
             weights = bounded_keelin_3_discrete_probabilities(zGrid, 0.16, d.z_lower, d.z, d.z_upper, d.z_lowerbound, d.z_upperbound, N=40, P=0.99999)
             
             if weights.ndim == 1:
@@ -260,27 +263,34 @@ class GalCompleted(object):
             
             weights *= d.w[:, np.newaxis]
             
-            # completness eval for each gal, on grid
-            completnesses = c.completeness(d.theta, d.phi, zGrid)
+            if self._additive and (len(self._galcats) == 1): # no need to evaluate completeness...
+                catweightTotal = w
                 
-            # for additive completion we do not divide by completness as opposed to otherwise (mult, mix)
-            # an overall factor of completeness from the weighted average
-            # over catalogs then survives but, in the case of 1 catalog, it is cancelling with total_completeness!
-            #
-            # We get this one more factor of completeness in the case of additive completion automatically from the confidence function, which returns its argument in the case of additive completion.
-            # in all other cases (mult, mix), the confidence is a probability of trust in pure multiplicative completion and is =1 in mult, and between 0 and 1 in mix.
+            else:
             
-            weights *= self.confidence(completnesses)
-            weights *= w
-          
-            weights /= self.total_completeness(d.theta, d.phi, zGrid)
-           
+                # completness eval for each gal, on grid - same shape as weights
+                completness = c.completeness(d.theta, d.phi, zGrid)
+                    
+                # multiplicative completion
+                weights /= completness
+                # downweighted for low completeness
+                weights *= self.confidence(completnesses)
+                
+                # catalog weighting based also on completeness
+                catweight = w*np.mean(completeness)
+                weights *= catweight
+                catweightTotal += catweight
+            
+            # normalize in case different goals are used for different catalogs, to make them comparable
             weights /= c._completeness._comovingDensityGoal
    
             allweights.append(weights)
             
+        allweights = np.vstack(allweights)
+        allweights /= catweightTotal
+            
         #return np.squeeze(np.vstack(allpixels)), np.vstack(allweights)
-        return np.hstack(allpixels), np.vstack(allweights)
+        return np.hstack(allpixels), allweights
     
 
     def get_inhom(self, nside):
@@ -317,23 +327,33 @@ class GalCompleted(object):
             redshifts = d.z.to_numpy()
             allredshifts.append(redshifts)
             
-            # completness eval for each gal
-            completnesses = c.completeness(d.theta, d.phi, redshifts, oneZPerAngle = True)
+            if self._additive and (len(self._galcats) == 1): # no need to evaluate completeness...
+                catweightTotal = w
                 
-            weights *= self.confidence(completnesses)
-            
-            weights *= w
-            
-          
-            weights /= self.total_completeness(d.theta, d.phi, redshifts, oneZPerAngle = True)
-       
+            else:
+           
+                # completness eval for each gal
+                completness = c.completeness(d.theta, d.phi, redshifts, oneZPerAngle = True)
+                   
+                # multiplicative completion
+                weights /= completness
+                # downweighted for low completeness
+                weights *= self.confidence(completnesses)
+               
+                # catalog weighting based also on completeness
+                catweight = w*np.mean(completeness)
+                weights *= catweight
+                catweightTotal += catweight
+           
+            # normalize in case different goals are used for different catalogs, to make them comparable
             weights /= c._completeness._comovingDensityGoal
-            
-            
+  
             allweights.append(weights)
-            
-        #return np.squeeze(np.vstack(allpixels)), np.squeeze(np.vstack(allredshifts)), np.squeeze(np.vstack(allweights))
-        return np.hstack(allpixels), np.hstack(allredshifts), np.hstack(allweights)
+           
+        allweights = np.hstack(allweights)
+        allweights /= catweightTotal
+                   
+        return np.hstack(allpixels), np.hstack(allredshifts), allweights
     
     def eval_inhom(self, Omega, z):
         '''
@@ -349,31 +369,28 @@ class GalCompleted(object):
             assert(len(theta) == len(z))
         
         ret = np.zeros(len(theta))
+        catweightTotal = 0
         
         for c, w in zip(self._galcats, self._catweights):
             
         
             # completness eval for each point
-            completnesses = c.completeness(theta, phi, z, oneZPerAngle = True)
+            completeness = c.completeness(theta, phi, z, oneZPerAngle = True)
             
-            # for catalog averaging (1)
-            retc = completnesses
             
             # how much of homogeneous stuff to add - note in case of additive completion, confidence returns its argument, and we have 1 - completness, the correct prefactor in that case
             
-            retc *= (1-self.confidence(completnesses))
+            retcat = (1-self.confidence(completeness))
             
-            # for catalog averaging (2)
-            retc *= w
+            # catalog weighting based also on completeness
+            catweight = w*np.mean(completeness)
+            retcat *= catweight
+            catweightTotal += catweight
             
-            # homogeneous density
-            #   divide by it - do not multiply here.
-            #   retc *= c._completeness._comovingDensityGoal
-            
-            ret += retc
+            ret += retcat
         
         # for catalog averaging (3)
-        ret /= self.total_completeness(theta, phi, z, oneZPerAngle=True)
+        ret /= catweightTotal
         return ret
         
         